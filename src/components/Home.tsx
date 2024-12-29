c4f63aff1650cfc2f302e8586f20a8d0e2db7956
shilcode <2850542@gmail.com>
2024-12-29 16:39:48 +0100

    more structure

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="w-screen">
+      <h1>Test Automation Demo Site</h1>
+      <br />
       <Link to="/login">Login Page</Link>
       <br />
       <Link to="/formpage">Forms</Link>
       <br />
       <Link to="/web-elements">Web Elements</Link>
+      <br />
+      <Link to="/Widgets">Widgets</Link>
       <br />
     </div>
   );
