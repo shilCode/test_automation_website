b8821cc2d9b2c5ef6018a528743bb49f5c3747e7
mohanshil <mohanshil05@gmail.com>
2024-12-30 19:22:52 +0100

    removed app.css file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home-page">
-      <h1 className="Test-header">Test Automation Demo Site</h1>
+      <h1 className="test-header">Test Automation Demo Site</h1>
       <br />
-      <div className="home-links">
+      <div className="home-links"  >
       <Link id="login" to="/login">Login Page</Link>
       <br />
       <Link id="login" to="/formpage">Forms</Link>
