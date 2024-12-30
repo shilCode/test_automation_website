b417390001c4ff585ddf8143d29ea933f30a4b02
shilcode <2850542@gmail.com>
2024-12-30 19:38:37 +0100

    fixing build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Forms from "./components/Forms";
