eda81807fec212740216433924e6f46c0190f06e
shilcode <2850542@gmail.com>
2024-12-30 20:29:56 +0100

    added ta resources

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,33 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home-page">
+    <div className="">
       <h1 className="test-header">Test Automation Demo Site</h1>
       <br />
-      <div className="home-links"  >
-      <Link id="login" to="/login">Login Page</Link>
+      <div className="home-links justify-between">
+        <Link id="login" to="/login">
+          Login Page
+        </Link>
+        <br />
+        <Link id="login" to="/formpage">
+          Forms
+        </Link>
+        <br />
+        <Link id="login" to="/web-elements">
+          Web Elements
+        </Link>
+        <br />
+        <Link id="login" to="/Widgets">
+          Widgets
+        </Link>
+        <br />
+      </div>
       <br />
-      <Link id="login" to="/formpage">Forms</Link>
       <br />
-      <Link id="login" to="/web-elements">Web Elements</Link>
       <br />
-      <Link id="login" to="/Widgets">Widgets</Link>
       <br />
+      <div>
+        <Link to="/taguides">Test Automation Resources</Link>
       </div>
     </div>
   );
